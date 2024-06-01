d7397f7b701a2cbeb597f88c7781660f020ade0a
EvaGithub <166377149+EvaGithub@users.noreply.github.com>
2024-06-01 19:34:01 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,32 +6,6 @@ import seaborn as sns
 from PIL import Image
 import base64
 import os
-import nltk
-from nltk.tokenize import word_tokenize
-import subprocess
-import sys
-
-# Now try importing NLTK
-try:
-    import nltk
-    nltk.download('punkt')
-    nltk.download('stopwords')
-    st.write('NLTK is successfully installed and data downloaded!')
-except ImportError as e:
-    st.error(f"Failed to import NLTK: {e}")
-
-
-# List installed packages to debug installation issues
-installed_packages = subprocess.run([sys.executable, "-m", "pip", "list"], capture_output=True, text=True)
-st.text(installed_packages.stdout)
-
-
-# Download necessary NLTK data
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
-
-
-nltk.download('punkt')
 
 # Function to convert image to base64 for HTML embedding
 def get_base64_image(image_path):
@@ -43,43 +17,44 @@ def get_base64_image(image_path):
         return None
 
 # Custom CSS to style the app
+# Custom CSS to style the app
 st.markdown("""
     <style>
         .main {
-            background-color: #ffffff;
+            background-color: #ffffff; /* White background for the main page */
         }
         .sidebar .sidebar-content {
-            background-color: #ADD8E6;
+            background-color: #ADD8E6; /* Light blue for the sidebar */
         }
         .reportview-container .main .block-container {
-            background-color: #ffffff;
+            background-color: #ffffff; /* White background for main content */
         }
         .css-18e3th9, .css-1d391kg {
-            background-color: #ADD8E6;
+            background-color: #ADD8E6; /* Light blue for the top menu bar */
         }
         .stMarkdown p {
-            color: #495057;
+            color: #495057; /* Dark grey for text */
         }
         .stButton > button {
-            background-color: #007bff;
-            color: #fff;
+            background-color: #007bff; /* Blue for buttons */
+            color: #fff; /* White text on buttons */
         }
         h1, h2, h3, h4, h5, h6 {
-            color: #343a40;
+            color: #343a40; /* Dark grey for headings */
         }
         .participants-title {
             font-size: 20px;
             font-weight: bold;
-            margin-top: 20px;
+            margin-top: 20px; /* Space above the participants title */
         }
         .header-container {
             position: relative;
             width: 100%;
             height: 250px;
-            background-size: cover;
-            background-position: center center;
-            background-repeat: no-repeat;
-            border-bottom: 2px solid white;
+            background-size: cover; /* Adjusted to cover */
+            background-position: center center; /* Centered horizontally and vertically */
+            background-repeat: no-repeat; /* Ensures the image doesn't repeat */
+            border-bottom: 2px solid white; /* Adds a white border line at the bottom */
         }
     </style>
 """, unsafe_allow_html=True)
@@ -124,65 +99,23 @@ elif selected_menu == "Problem":
     base64_image = get_base64_image(image_path)
     if base64_image:
         st.markdown(f"""<div class="header-container" style="background-image: url('data:image/jpeg;base64,{base64_image}');"></div>""", unsafe_allow_html=True)
+    
     st.markdown("""
+        
         ### Challenge Overview:
         - Classify products in Rakuten's e-commerce catalog using text and images.
         - Multimodal classification problem; improves product categorization.
         ### Data Source:
-        - Rakuten France Multimodal Product Data Classification Challenge. [Download Link](https://challengedata.ens.fr/challenges/35)
+        - Rakuten France Multimodal Product Data Classification Challenge.  [Download Link](https://challengedata.ens.fr/challenges/35)
         ### Validation:
         - Competition site for validation and ranking. [Validation Link](https://challengedata.ens.fr/participants/challenges/35/ranking/public)
     """)
 
+
+# Placeholder for other sections
 if selected_menu == "Data":
     st.title("Data")
-    if X_train is not None and Y_train is not None:
-        X_train['description'].fillna('No description', inplace=True)
-        X_train['designation_length'] = X_train['designation'].apply(lambda x: len(word_tokenize(x)))
-        X_train['description_length'] = X_train['description'].apply(lambda x: len(word_tokenize(x)))
-        train_data = X_train.join(Y_train)
-
-        st.sidebar.subheader("Visualization Options")
-        
-        # Visualization 1: Frequency Distribution of Product Type Codes
-        st.sidebar.subheader("Product Type Frequency Distribution")
-        all_classes = st.sidebar.checkbox("Show All Classes", True)
-        selected_classes = st.sidebar.multiselect("Select Classes", options=train_data['prdtypecode'].unique(), default=train_data['prdtypecode'].unique())
-        
-        if all_classes or not selected_classes:
-            filtered_data = train_data
-        else:
-            filtered_data = train_data[train_data['prdtypecode'].isin(selected_classes)]
-        
-        fig, ax = plt.subplots()
-        sns.countplot(y='prdtypecode', data=filtered_data, color="lightblue")
-        plt.title("Frequency Distribution of Product Type Codes")
-        st.pyplot(fig)
-        
-        # Visualization 2: Boxplot of Designation and Description Lengths
-        st.sidebar.subheader("Designation and Description Lengths")
-        show_designation = st.sidebar.checkbox("Show Designation Lengths", True)
-        show_description = st.sidebar.checkbox("Show Description Lengths", True)
-        
-        fig, ax = plt.subplots()
-        if show_designation:
-            sns.boxplot(x='prdtypecode', y='designation_length', data=train_data, color="green", ax=ax)
-        if show_description:
-            sns.boxplot(x='prdtypecode', y='description_length', data=train_data, color="red", ax=ax)
-        plt.title("Boxplot of Designation and Description Lengths")
-        plt.xticks(rotation=90)
-        st.pyplot(fig)
-        
-        # Visualization 3: Missing Descriptions by Product Type Code
-        st.sidebar.subheader("Missing Descriptions Analysis")
-        missing_data = train_data.copy()
-        missing_data['missing_description'] = missing_data['description'] == 'No description'
-        
-        fig, ax = plt.subplots()
-        sns.countplot(x='prdtypecode', hue='missing_description', data=missing_data)
-        plt.title("Missing Descriptions by Product Type Code")
-        plt.xticks(rotation=90)
-        st.pyplot(fig)
+    st.markdown("Details about the data analysis go here.")
 
 if selected_menu == "Models":
     st.title("Models")
@@ -195,4 +128,3 @@ if selected_menu == "Results":
 if selected_menu == "Future work":
     st.title("Future work")
     st.markdown("Details about the future work go here.")
-
